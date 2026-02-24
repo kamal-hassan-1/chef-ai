6aa85ca77ada03c9ff11888cd5a70a7bc192ee36
kamal-hassan-1 <scienceknowledge191andme@gmail.com>
2026-02-25 00:44:46 +0500

    new func

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ export default function App() {
 	return (
 		<>
 			<Header />
-			<AddIngredient />
+			<main>
+				<AddIngredient />
+			</main>
 		</>
 	);
 }
--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -3,7 +3,7 @@ export default function AddIngredient() {
 	const [ingredients, setIngredients] = React.useState([]);
 
 	const populateIngredientsListItems = () => {
-		return ingredients.map((ingredient) => <li>{ingredient}</li>);
+		return ingredients.map((ingredient) => <li className="mb-1">{ingredient}</li>);
 	};
 
 	function handleSubmit(event) {
@@ -15,12 +15,19 @@ export default function AddIngredient() {
 	}
 
 	return (
-		<main className="px-8 pt-2.5">
-			<form onSubmit={handleSubmit} className="add-ingredient-form">
-				<input className="add-ingredient-form-input" type="text" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient" />
-				<button className="add-ingredient-form-button cursor-pointer">Add ingredient</button>
-			</form>
-			<ul>{populateIngredientsListItems()}</ul>
+		<main className="px-8 pt-2.5 flex flex-col justify-start">
+			<section id="add ingredients form">
+				<form onSubmit={handleSubmit} className="add-ingredient-form">
+					<input className="add-ingredient-form-input" type="text" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient" />
+					<button className="add-ingredient-form-button cursor-pointer">Add ingredient</button>
+				</form>
+			</section>
+			{ingredients.length != 0 && (
+				<section className="m-10">
+					<h2 className="text-2xl sm:text-3xl font-semibold mt-1">Ingredients on hand:</h2>
+					<ul className="text-gray-600 text-[1rem] list-disc ml-6 mt-2.5">{populateIngredientsListItems()}</ul>
+				</section>
+			)}
 		</main>
 	);
 }
