731281e087596ed3dbe78cf81c98b6976e9f1e8a
kamal-hassan-1 <scienceknowledge191andme@gmail.com>
2026-02-26 21:36:44 +0500

    get a recipe placeholder

--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -19,10 +19,19 @@ export default function AddIngredient() {
 					<button className="add-ingredient-form-button cursor-pointer">Add ingredient</button>
 				</form>
 			</section>
-			{ingredients.length != 0 && (
+			{ingredients.length > 0 && (
 				<section className="m-10">
-					<h2 className="text-2xl sm:text-3xl font-semibold">Ingredients on hand:</h2>
-					<ul className="text-gray-600 text-[1rem] list-disc ml-6 mt-2.5">{populateIngredientsListItems()}</ul>
+					<div id="ingredient-list-container">
+						<h2 className="text-2xl sm:text-3xl font-semibold">Ingredients on hand:</h2>
+						<ul className="text-gray-600 text-[0.875rem] list-disc ml-6 mt-2.5">{populateIngredientsListItems()}</ul>
+					</div>
+					<div className="generate-recipe-container flex justify-between items-center rounded-lg bg-[#F0EFEB] px-7 py-3 mt-9">
+						<div>
+							<h3 className="text-[1.125rem] font-medium leading-6 mb-2">Ready for a recipe?</h3>
+							<p className="text-[#6B7280] text-[0.875rem] leading-5">Generate a recipe from your list of ingredients.</p>
+						</div>
+						<button className="border-none rounded-md bg-[#D17557] shadow-sm text-[#FAFAF8] px-4 py-2.5 text-[0.875rem] cursor-pointer">Get a recipe</button>
+					</div>
 				</section>
 			)}
 		</main>
